b02852b75152fc501a29e9b168e8ec98ae3cbb52
hyuncoding <hyunstwolte@gmail.com>
2023-10-03 23:29:49 +0900

    전체게시판에 목록 뿌리기 구현 완료

--- src/main/java/com/app/ptever/controller/CommunityController.java
+++ src/main/java/com/app/ptever/controller/CommunityController.java
@@ -1,20 +1,38 @@
 package com.app.ptever.controller;
 
+import com.app.ptever.domain.dto.PostDTO;
+import com.app.ptever.domain.vo.PostVO;
+import com.app.ptever.repository.CommunityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/community/*")
+@RestController
 public class CommunityController {
+    private final CommunityService communityService;
 
 //    전체 게시판
     @GetMapping("full-page")
-    public void GoToFullPage(){;}
+    public ModelAndView GoToFullPage(){
+        ModelAndView mav = new ModelAndView();
+        List<PostDTO> allPosts = communityService.findAll();
+        mav.addObject("allPosts", allPosts);
+        log.info(mav.toString());
+        return mav;
+    }
 
 //    자유 게시판
     @GetMapping("free-post")
@@ -31,7 +49,7 @@ public class CommunityController {
 //    글쓰기
 
     @GetMapping("write")
-    public void GoToWrite(){;}
+    public void GoToWrite(HttpSession session){;}
 
 //    소도구 거래 게시판
     @GetMapping("transaction")
