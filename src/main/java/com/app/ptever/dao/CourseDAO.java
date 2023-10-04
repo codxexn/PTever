8541fdb1b4a9dcfe2aa7b1a6b5c7fb5a38a206f1
dxexn <codxeun@gmail.com>
2023-10-05 01:03:25 +0900

    courseReview DAO Test까지 완료

--- src/main/java/com/app/ptever/dao/CourseDAO.java
+++ src/main/java/com/app/ptever/dao/CourseDAO.java
@@ -1,11 +1,13 @@
 package com.app.ptever.dao;
 
 import com.app.ptever.domain.dto.CourseDTO;
+import com.app.ptever.domain.dto.ReviewDTO;
 import com.app.ptever.mapper.CourseMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,5 +19,9 @@ public class CourseDAO {
     //강의 상세보기
     public Optional<CourseDTO> findCourseById(Long courseId) {
         return courseMapper.selectById(courseId);
+    }
+
+    public List<ReviewDTO> findAllReviewByCourseId(Long courseId) {
+        return courseMapper.selectAllReview(courseId);
     }
 }
--- src/main/java/com/app/ptever/mapper/CourseMapper.java
+++ src/main/java/com/app/ptever/mapper/CourseMapper.java
@@ -1,8 +1,10 @@
 package com.app.ptever.mapper;
 
 import com.app.ptever.domain.dto.CourseDTO;
+import com.app.ptever.domain.dto.ReviewDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -10,4 +12,6 @@ public interface CourseMapper {
 
 //    강의 아이디로 강의 상세정보 조회
     public Optional<CourseDTO> selectById(Long courseId);
+
+    public List<ReviewDTO> selectAllReview(Long courseId);
 }
