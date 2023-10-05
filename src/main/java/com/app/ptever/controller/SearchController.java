79ff4899cf5bd4367c3ba8014838fb9530f56471
infiniment <smnoh0707@gmail.com>
2023-10-06 02:29:16 +0900

    검색 결과 페이지 작성

---
+++ src/main/java/com/app/ptever/controller/SearchController.java
@@ -0,0 +1,17 @@
+package com.app.ptever.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/search-results/*")
+public class SearchController {
+
+    @GetMapping("search")
+    public void GoToSearchResult(){;}
+}
