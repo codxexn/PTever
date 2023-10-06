0747197fdf866595c8f6c793c8fd8c380d0d0943
hyuncoding <hyunstwolte@gmail.com>
2023-10-06 17:48:03 +0900

    다양한 버그 방지 장치들 추가 완료/ 글쓰기 페이지(2개) 완료 / 게시물 수정, 삭제 + 댓글 삭제 기능 완료

--- src/main/resources/static/js/details/details.js
+++ src/main/resources/static/js/details/details.js
@@ -55,6 +55,24 @@ imageButton.forEach(image => {
     image.addEventListener('click', handleImageClick);
 });
 
+// 게시물 수정 클릭 시 실행 함수
+const updatePostId = document.querySelector(".detailContent-UpdateButton input");
+function updatePost(seq) {
+    let f = document.createElement('form');
+
+    let obj;
+    obj = document.createElement('input');
+    obj.setAttribute('type', 'hidden');
+    obj.setAttribute('name', 'postId');
+    obj.setAttribute('value', updatePostId.value);
+
+    f.appendChild(obj);
+    f.setAttribute('method', 'post');
+    f.setAttribute('action', '/community/detail-update');
+    document.body.appendChild(f);
+    f.submit();
+}
+
 // 게시물 삭제 클릭 시 실행 함수
 const postId = document.querySelector(".detailContent-DeleteButton input");
 function deletePost(seq){
@@ -81,6 +99,45 @@ function deletePost(seq){
             f.appendChild(obj);
             f.setAttribute('method', 'post');
             f.setAttribute('action', '/community/detail-delete');
+            document.body.appendChild(f);
+            f.submit();
+        }
+    })
+}
+
+// 댓글 삭제 버튼 클릭 시 실행 함수
+const communityCommentId = document.querySelector(".detailContent-comment-DeleteButton input");
+function deleteComment(seq){
+    Swal.fire({
+        title: '댓글을 삭제하시겠습니까?',
+        text: "삭제하신 댓글은 복구할 수 없습니다.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '삭제',
+        cancelButtonText: '취소'
+    }).then((result) => {
+        if (result.value) {
+            //"삭제" 버튼을 눌렀을 때 작업할 내용을 이곳에 넣어주면 된다.
+            let f = document.createElement('form');
+
+            let obj;
+            obj = document.createElement('input');
+            obj.setAttribute('type', 'hidden');
+            obj.setAttribute('name', 'communityCommentId');
+            obj.setAttribute('value', communityCommentId.value);
+
+            let obj2;
+            obj2 = document.createElement('input');
+            obj2.setAttribute('type', 'hidden');
+            obj2.setAttribute('name', 'postId');
+            obj2.setAttribute('value', postId.value);
+
+            f.appendChild(obj);
+            f.appendChild(obj2);
+            f.setAttribute('method', 'post');
+            f.setAttribute('action', '/community/delete-comment');
             document.body.appendChild(f);
             f.submit();
         }
