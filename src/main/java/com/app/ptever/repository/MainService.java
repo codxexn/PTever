1f88cbaa82a53b7f1fee2e3840f3e475b4a0da1b
infiniment <smnoh0707@gmail.com>
2023-10-10 02:05:01 +0900

    메인페이지에 소도구 게시판 이미지 불러오기 완료

--- src/main/java/com/app/ptever/repository/MainService.java
+++ src/main/java/com/app/ptever/repository/MainService.java
@@ -2,6 +2,8 @@ package com.app.ptever.repository;
 
 import com.app.ptever.domain.dto.CourseSelectDTO;
 import com.app.ptever.domain.dto.PostDTO;
+import com.app.ptever.domain.dto.ShoppingMallSelectDTO;
+import com.app.ptever.domain.vo.PostImgVO;
 
 import java.util.List;
 
@@ -12,6 +14,11 @@ public interface MainService {
     // 강의 최신순으로 가져오기
     public List<CourseSelectDTO> getLatestDateCourseList();
 
+    public List<ShoppingMallSelectDTO> getRegisterDateProductList();
+
     // 최신순으로 개별 게시판 게시물 조회(메인에는 소도구 거래만)
     public List<PostDTO> getLatestDateCommunityList();
+
+    // postId로 이미지들 가져오기
+    public List<PostImgVO> findAllByPostId(Long postId);
 }
